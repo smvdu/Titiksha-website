04143a53aadd3fd418cbcf58dddf34fb2b510bb3
Saurabh Kumar <saurabh@saurabhworld.in>
2012-02-20 12:37:45 +0530

    assest created

---
+++ discussions/assests/js/script.js
@@ -0,0 +1,11 @@
+/* Author: Saurabh Kumar
+   Last Modified: Feb 20, 2012
+****************************/
+
+var debug = false;
+
+$(document).ready(function(){
+
+	var frame = $('#containter-frame');
+
+});
