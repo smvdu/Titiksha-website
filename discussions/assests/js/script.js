c2f7e64a10f38c1f8233d024f9209380895bc6aa
Saurabh Kumar <saurabh@saurabhworld.in>
2012-02-20 14:10:27 +0530

    fixed iframe autoresize

--- discussions/assests/js/script.js
+++ discussions/assests/js/script.js
@@ -2,10 +2,29 @@
    Last Modified: Feb 20, 2012
 ****************************/
 
-var debug = false;
+var debug = true;
 
 $(document).ready(function(){
 
+	log("App started");
 	var frame = $('#containter-frame');
 
+	//hide document default scroll-bar
+	document.body.style.overflow = 'hidden';
+
+		
+	frame.load(resizeIframe);	
+	$(window).resize(resizeIframe); 
+
+	function resizeIframe() {
+    	var h = $(window).height();
+    	var w = $(window).width();
+    	frame.height(h);
+    	frame.width(w);
+	}
 });
+
+function log (str) {
+		// debug log event...
+	if (debug) {console.log(str);};	
+}
