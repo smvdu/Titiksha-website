013826aba5f27783462722f18c2fae9f20a4f19b
Saurabh Kumar <saurabh@saurabhworld.in>
2012-03-06 09:30:53 +0530

    quick links

--- php/config.php
+++ php/config.php
@@ -1,4 +1,5 @@
 <?php	
+	//site meta datas goes here
 	define("site_name","Titiksha",1);
 	define("site_title", "Titiksha | Annual tech-fest of Shri Mata Vaishno Devi University", 1);
 	define("site_type", "Organisation",1);
@@ -9,11 +10,13 @@
 	define('site_photo_url', "");
 	define('site_author', 'Saurabh Kumar and contributors (https://github.com/SaurabhKumar/titiksha-website',1);
 	
-	define('site_twitter_url', "https://twitter.com/titikshasmvdu",1);
-	define('site_forum_url', "http://groups.google.com/group/titiksha",1);
+	//add links here
+	define('site_twitter_url', "https://twitter.com/titikshasmvdu",1);	
 	define('site_url',"http://titiksha.smvdu.net.in",1);
-	define('site_fb_appid','',1);
+	define('site_forum_url', "http://titiksha.smvdu.net.in/discussions",1);
 	
+
+	define('site_fb_appid','',1);	
 	define('analytics_id', 'UA-16351077-7',1);
 	
 	
