e7c97628165a2bddfa9409e2562e58e540eed208
Saurabh Kumar <saurabh@saurabhworld.in>
2012-03-06 12:39:18 +0530

    titiksha2012 url

--- php/config.php
+++ php/config.php
@@ -14,7 +14,7 @@
 	define('site_twitter_url', "https://twitter.com/titikshasmvdu",1);	
 	define('site_url',"http://titiksha.smvdu.net.in",1);
 	define('site_forum_url', "http://titiksha.smvdu.net.in/discussions",1);
-	define('site_titiksha2012_url', site_url."/2012",1);
+	define('site_titiksha2012_url', "http://2012.titiksha.smvdu.net.in",1);
 	define('site_register_url', site_titiksha2012_url."/register",1);
 
 	
