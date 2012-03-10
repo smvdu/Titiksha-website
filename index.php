903ad7231ad10a94e211c8e540cbe68500d70025
Rishabh Shukla <rishabhshukla25@yahoo.com>
2012-03-10 19:43:23 +0530

    changed text color of lifestream and visited links

--- index.php
+++ index.php
@@ -140,18 +140,7 @@
           </div>
         </div>
 
-        <div class="page-header">
-          <h1>Our Previous Sponsors</h1>
-          
-        </div>
-
-        <div class="row">
-          <div class="span16 icons sponsor-list">
-              <?php get_prev_sponsors_list(); ?>
-          </div>
-        </div>
-
-      </section>
+     </section>
 
 
       <footer>
