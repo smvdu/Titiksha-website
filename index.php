684104e17e0a1395e4803f230a4685eb26384a60
sunnyLA4 <sunnylautner4@gmail.com>
2013-09-08 04:57:28 +0530

    code added to indx

--- index.php
+++ index.php
@@ -27,12 +27,22 @@
 	    <meta name="author" content="<?php print(site_author);?>">
 		
 	  	<?php get_fb_meta(); ?>
-		
+	  	<!-- css for menu.php -->
+	  	<link rel="stylesheet" type="text/css" href="css/demo.css" />
+        <link rel="stylesheet" type="text/css" href="css/style.css" />
+        <link href='http://fonts.googleapis.com/css?family=Terminal+Dosis' rel='stylesheet' type='text/css' />
+		<!-- ======================== -->
 		<link rel="shortcut icon" href="intersection.ico"> 
 		<link rel="stylesheet" type="text/css" href="css/default.css" />
 		<link rel="stylesheet" type="text/css" href="css/component.css" />
 		<link rel="stylesheet" href="css/normalize.css">
         <link rel="stylesheet" href="css/main.css">
+
+        <!--  Timer css scripts   !-->
+        <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Open+Sans+Condensed:300" />
+        <link rel="stylesheet" href="css/styles.css" />
+        <link rel="stylesheet" href="css/jquery.countdown.css" />
+
         <script src="js/vendor/modernizr-2.6.2.min.js"></script>
         <script type="text/javascript" src="js/PxLoader.js"></script>
         <script type="text/javascript" src="js/PxLoaderImage.js"></script>
@@ -96,11 +106,87 @@
 
 
 			</div>
-		</div><!-- /container -->
+		</div><!-- /container of loader -->
+		<!-- added menu here -->
+		<div class="container_menu">
+            <img src="img/3.jpg" id="bg"/>
+            <div class="header">
+                <div id="h-text">
+                    <p>Titiksha 2k13</p>
+                </div>
+            </div>
+            
+            <div class="content">
+                <ul class="ca-menu">
+                    <li>
+                        <a href="#">
+                            <span class="ca-icon">F</span>
+                            <div class="ca-content">
+                                <h2 class="ca-main">Events</h2>
+                                <h3 class="ca-sub">Personalized to your needs</h3>
+                            </div>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <span class="ca-icon">H</span>
+                            <div class="ca-content">
+                                <h2 class="ca-main">Workshop</h2>
+                                <h3 class="ca-sub">Advanced use of technology</h3>
+                            </div>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <span class="ca-icon" id="heart">N</span>
+                            <div class="ca-content">
+                                <h2 class="ca-main">Competitions</h2>
+                                <h3 class="ca-sub">Understanding visually</h3>
+                            </div>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <span class="ca-icon">K</span>
+                            <div class="ca-content">
+                                <h2 class="ca-main">Register Now</h2>
+                                <h3 class="ca-sub">Professionals in action</h3>
+                            </div>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <span class="ca-icon">L</span>
+                            <div class="ca-content">
+                                <h2 class="ca-main">About Us</h2>
+                                <h3 class="ca-sub">24/7 for you needs</h3>
+                            </div>
+                        </a>
+                    </li>
+                </ul>
+                <div id="countdown"></div>
+                <p id="note"></p>
+                
+            </div><!-- content -->
+            <div class="footer">
+                <a target="_blank" href="https://www.facebook.com/titiksha2k13"><img src="img/fb.png"></a> 
+                <span>&nbsp &nbsp</span>
+                <img src="img/twi.png"></span>
+            </div>
+        </div><!-- container_menu -->
 
-		
+
+		<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js"></script>
+        
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+		
 		<script>window.jQuery || document.write('<script src="js/vendor/jquery-1.9.1.min.js"><\/script>')</script>
+        
+		 <!-- JavaScript includes -->
+        <script src="http://code.jquery.com/jquery-1.7.1.min.js"></script>
+        <script src="js/jquery.countdown.js"></script>
+        <script src="js/script.js"></script>
+
         <script src="js/plugins.js"></script>
         <script src="js/main.js"></script>
 		<script src="js/jquery.lettering.js"></script>
