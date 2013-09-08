ce24a75e476ccbbd4b4aab881a796e23163ba733
sunnyLA4 <sunnylautner4@gmail.com>
2013-09-09 00:39:30 +0530

    repo cleaned

--- contact.php
+++ contact.php
@@ -29,7 +29,6 @@
 	  	<?php get_fb_meta(); ?>
 		
 		<link rel="shortcut icon" href="intersection.ico"> 
-		<link rel="stylesheet" type="text/css" href="css/component.css" />
 		<link rel="stylesheet" href="css/normalize.css">
         <link rel="stylesheet" href="css/main.css">
         <script src="js/vendor/modernizr-2.6.2.min.js"></script>
