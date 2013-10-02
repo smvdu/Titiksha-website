e51c193fdebc550ce78defac5b23ae8f8d559ec6
procoderx <rishy.s13@gmail.com>
2013-10-03 02:18:57 +0530

    added waypoints and navigation on menu page

--- js/main.js
+++ js/main.js
@@ -2,6 +2,38 @@ $(document).ready(function(){
 	//creates a stellar object
 	$.stellar();
 
+	jQuery.easing.def = "easeInOutQuint";
+	var htmlbody = $('html,body');
+	var links = $(".ca-menu").find('li');
+
+
+	//Create a function that will be passed a slide number and then will scroll to that slide using jquerys animate. The Jquery
+    //easing plugin is also used, so we passed in the easing method of 'easeInOutQuint' which is available throught the plugin.
+    function goToByScroll(dataslide) {
+        htmlbody.animate({
+            scrollTop: $('.page[data-slide="' + dataslide + '"]').offset().top
+        }, dataslide * 1300);
+    }
+
+
+    //When the user clicks on the navigation links, get the data-slide attribute value of the link and pass that variable to the goToByScroll function
+    links.click(function (e) {
+        e.preventDefault();
+        dataslide = $(this).attr('data-slide');
+        goToByScroll(dataslide);
+    });
+
+
+    //When the user clicks on the button, get the get the data-slide attribute value of the button and pass that variable to the goToByScroll function
+    /*button.click(function (e) {
+        e.preventDefault();
+        dataslide = $(this).attr('data-slide');
+        goToByScroll(dataslide);
+    });*/
+
+
+
+
 	// events squeeze and expand effect
 
 	$("#event").on("click","div",function(event) {
