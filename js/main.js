20fc94878678a3f13c7ab2125a7749b6a90f2396
AKSHAYPRATAP007 <pratapakshay0@gmail.com>
2013-10-03 00:19:36 +0530

    effects added in event page

--- js/main.js
+++ js/main.js
@@ -6,16 +6,21 @@ $(document).ready(function(){
 
 	$("#event").on("click","div",function(event) {
 		// Reset all properties
+		$(".event_heading").css("top","0px");
 		$(".event_content").css("display","none");
 		$("#event div").removeClass("expand squeeze current_heading");
 		
 		// set new properties
 		$(this).addClass("expand");
+		$('#event>div').not(this).addClass("squeeze");
 		var className=$(this).attr("class");
 		var thisClass=className.split(' ');
-		$("."+thisClass[1]+">.event_heading").removeClass("expand").addClass("current_heading");
-		$("."+thisClass[1]+">.event_content").css("display","block");
-		$('#event>div').not(this).addClass("squeeze");
-	})
+		$("."+thisClass[1]+">.event_heading").removeClass("expand").css("top","-230px",function(){
+			$("."+thisClass[1]+">.event_heading").addClass("current_heading");
+		});
+		$("."+thisClass[1]+">.event_content").fadeIn(3000).css("display","block");
+		
+		
+	});
 });
 
