4b127af81d1ac28b6e819829cdf0a1cf5307df29
AKSHAYPRATAP007 <pratapakshay0@gmail.com>
2013-10-16 01:16:57 +0530

    deleted unused files

--- php/query/php/table.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS `admin` (
-  `id` int(255) NOT NULL AUTO_INCREMENT,
-  `username` varchar(50) NOT NULL,
-  `password` varchar(50) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `username` (`username`)
-) ENGINE=InnoDB COLLATE=utf8_unicode_ci;
-
