2b89aebdfa29f2acfefad631de6427e40ff934d1
codingeass <amandeeptheviper@gmail.com>
2014-10-09 18:29:49 +0530

    Added registration checking file

---
+++ public/js/app/register.js
@@ -0,0 +1,26 @@
+$('#button_r').click(function(){
+
+var name= $("#name").val();
+var email=$("#email").val();
+var pass=$("#password").val();
+var repass=$("#repassword").val();
+var phone=$('#phone').val();
+var coll=$('#college').val();
+	if (pass.length<6) {
+		$('#register_status').html("Password is less than 6 character");
+		return;
+	};
+	if(pass!=repass)
+	{
+		$('#register_status').html("Password Don't match");
+		return;
+	}
+		
+	$.ajax({
+		url :'',
+		data:'na='+name+'&em='+email+'&pas='+pass+'&pho='+phone+'&col='+coll,
+		success:function(data){
+			$('#register_status').html(data);
+		}
+	});
+});
