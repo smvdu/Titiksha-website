dfa50e8247b8aa6884c5a377652f9cab74cb0706
codingeass <amandeeptheviper@gmail.com>
2014-10-09 18:02:26 +0530

    connect file for mysql added

---
+++ public/php/connect.php
@@ -0,0 +1,14 @@
+<?php
+	/**
+ * MYSQL Cerendentials Include Page
+ *
+ * @author Amandeep Gupta <amandeeptheviper@gmail.com>
+ */
+			$mysql=mysql_connect("","","") 
+			or die ("Cannot connect to database");
+			if (mysqli_connect_errno())
+			{
+				echo "Failed to connect to MySQL: " . mysqli_connect_error();
+			}
+			$db = mysql_select_db("fest",$mysql);
+?>
