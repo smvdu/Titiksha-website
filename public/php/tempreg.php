9cb77f6749588cb12297ce828d9213c1b77fa96b
codingeass <amandeeptheviper@gmail.com>
2014-10-10 19:09:09 +0530

    updated tempreg

--- public/php/tempreg.php
+++ public/php/tempreg.php
@@ -4,7 +4,7 @@ try{
 	include("connect.php");
 	if(isset($_REQUEST["na"])&&isset($_REQUEST["em"])&&isset($_REQUEST["pas"])&&isset($_REQUEST["pho"])&&isset($_REQUEST["col"]))
 	{
-		if($_REQUEST["na"]!="" && $_REQUEST["em"]!="" && $_REQUEST["pas"]!="" && $_REQUEST["pho"]!=""&&$_REQUEST["col"])
+		if(strip_tags($_REQUEST["na"])!="" && strip_tags($_REQUEST["em"])!="" && strip_tags($_REQUEST["pas"])!="" && strip_tags($_REQUEST["pho"])!=""&&strip_tags($_REQUEST["col"])!="")
 		{
 			$confirm_code=md5(uniqid(rand()));
 			$query="Insert into tempaccount (confirm_code,username,password,email,phone,college)values('".$confirm_code."','".strip_tags($_REQUEST["na"])."','".md5(strip_tags($_REQUEST["pas"]))."','".strip_tags($_REQUEST["em"])."','".strip_tags($_REQUEST["pho"])."','".strip_tags($_REQUEST["col"])."')";
