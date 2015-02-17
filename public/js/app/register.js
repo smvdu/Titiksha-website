8ac17059dff80d4d01debc4a85d104bb16e84b03
codingeass <amandeeptheviper@gmail.com>
2015-02-17 19:54:40 +0530

    register button added on every event

--- public/js/app/register.js
+++ public/js/app/register.js
@@ -114,3 +114,30 @@ function disable_button()
 		}	
 	}
 }
+
+function event_register(id)
+{
+	if(id!="")
+	{
+		var xmlhttp=false;
+		if (window.XMLHttpRequest)
+		{// code for IE7+, Firefox, Chrome, Opera, Safari
+			xmlhttp=new XMLHttpRequest();
+		}
+		else
+		{// code for IE6, IE5
+			xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+		}
+		if(xmlhttp)
+		{
+			
+			xmlhttp.open("GET",'public/php/event.php?event_id='+id);
+			xmlhttp.onreadystatechange=function()
+			{
+				if(xmlhttp.readyState==4 && xmlhttp.status==200)
+					alert(""+xmlhttp.responseText);		
+			}
+			xmlhttp.send();
+		}
+	}
+}
