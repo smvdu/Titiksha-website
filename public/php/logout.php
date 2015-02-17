803cf8b34bb98b1f9e292587a86abd86576a1d96
codingeass <amandeeptheviper@gmail.com>
2015-02-17 21:55:40 +0530

    destroying cookie on logout

--- public/php/logout.php
+++ public/php/logout.php
@@ -1,7 +1,8 @@
 <?php
 session_start();
 session_unset(); 
-
+setcookie("em","",time()-3600,"/");
+setcookie("PHPSESSID","",time()-3600,"/");
 // destroy the session 
 session_destroy();
 echo "done";
