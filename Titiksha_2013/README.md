9dc970244dca50e2a3d36186870b849387f14464
shyam2tayal <shyam2tayal@gmail.com>
2015-09-11 22:13:52 +0530

    Removed old files

--- Titiksha_2013/README.md
+++
@@ -1,2 +0,0 @@
-# [Titiksha 2k13](http://titiksha.smvdu.net.in/)
-Titiksha is annual tech-fest of Shri Mata Vaishno Devi University
--- titiksha_2015/public/php/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-session_unset(); 
-setcookie("em","",time()-3600,"/");
-setcookie("PHPSESSID","",time()-3600,"/");
-// destroy the session 
-session_destroy();
-echo "done";
-?>
